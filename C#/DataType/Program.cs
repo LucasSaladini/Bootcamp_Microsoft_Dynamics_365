8d33fcbeeb2a1e069905ea0b97f2f15566a00cca
Lucas Saladini <lucassaladini@hotmail.com>
2023-04-15 12:21:29 -0300

    Working with data types

---
+++ C#/DataType/Program.cs
@@ -0,0 +1,11 @@
+﻿string apresentacao = "Olá, seja bem vindo";
+int quantidade = 1;
+double altura = 1.80;
+decimal preco = 1.90M;
+bool condicao = true;
+
+Console.WriteLine(apresentacao);
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+Console.WriteLine("Valor da variável preco: " + preco);
+Console.WriteLine("Valor da variável condicao: " + condicao);
