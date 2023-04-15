2e2712ddb96fad062c81483865b117c53dbb9807
Lucas Saladini <lucassaladini@hotmail.com>
2023-04-15 11:45:10 -0300

    Add new property

--- C#/Project_02/models/Pessoa.cs
+++ C#/Project_02/models/Pessoa.cs
@@ -9,6 +9,7 @@ namespace Project_02.Models
     {
         public string Nome { get; set; }
         public int Idade { get; set; }
+        public string NomeRepresentanteLegalDaPessoaFisica { get; set; }
 
         public void Apresentar()
         {
