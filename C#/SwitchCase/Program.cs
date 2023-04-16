98dcece718ca9f30526967b8f0410f24f9964161
Lucas Saladini <lucassaladini@hotmail.com>
2023-04-16 11:05:44 -0300

    Using switch to validate if it is a vowel or not

---
+++ C#/SwitchCase/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Digite uma letra");
+
+string letra = Console.ReadLine();
+
+switch (letra)
+{
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+        Console.WriteLine("Vogal");
+        break;
+    default:
+        Console.WriteLine("Não é uma vogal");
+        break;
+}
