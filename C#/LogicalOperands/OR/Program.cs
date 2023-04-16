82a9a3e96f09bc0de07367adb4168094890790a2
Lucas Saladini <lucassaladini@hotmail.com>
2023-04-16 11:24:52 -0300

    Using OR operand to validate conditions

---
+++ C#/LogicalOperands/OR/Program.cs
@@ -0,0 +1,11 @@
+﻿bool eMaiorDeIdade = false;
+bool possuiAutorizacaoDoResponsalvel = false;
+
+if (eMaiorDeIdade || possuiAutorizacaoDoResponsalvel)
+{
+    Console.WriteLine("Entrada liberada!");
+}
+else
+{
+    Console.WriteLine("Entrada não liberada");
+}
