6c780c4dbf441cde30b8c17c77cde300802d4b0f
Lucas Saladini <lucassaladini@hotmail.com>
2023-04-16 10:05:32 -0300

    Converting int to string

--- C#/AtributionOperands/Program.cs
+++ C#/AtributionOperands/Program.cs
@@ -6,12 +6,17 @@ c = c + 5;
 c += 5;
 c -= 5;
 
-Console.WriteLine(c);
+Console.WriteLine(c + " é o valor das operações da variável c");
 
 
 
 int d = Convert.ToInt32("5");
 int e = int.Parse("5");
 
-Console.WriteLine(d);
-Console.WriteLine(e);
+Console.WriteLine(d + " é o valor da variável d");
+Console.WriteLine(e + " é o valor da variável e");
+
+int inteiro = 5;
+string f = inteiro.ToString();
+
+Console.WriteLine(f + " é o valor da variável f");
