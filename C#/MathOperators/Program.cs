fe03da1cc2e534c2e72396ea0400eeb55968c52b
Lucas Saladini <lucassaladini@hotmail.com>
2023-04-18 19:00:01 -0300

    Added increment and decrement

--- C#/MathOperators/Program.cs
+++ C#/MathOperators/Program.cs
@@ -10,3 +10,15 @@ calc.Power(3, 3);
 calc.Sin(30);
 calc.Cos(30);
 calc.Tan(30);
+
+int numberIncrement = 10;
+int numberDecrement = 20;
+
+Console.WriteLine(numberIncrement);
+Console.WriteLine("Incrementando o 10");
+numberIncrement++;
+Console.WriteLine(numberIncrement);
+Console.WriteLine(numberDecrement);
+Console.WriteLine("Decrementando o 20");
+numberDecrement--;
+Console.WriteLine(numberDecrement);
