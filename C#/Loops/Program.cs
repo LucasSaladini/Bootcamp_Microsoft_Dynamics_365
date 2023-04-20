f38c549ad60d5dd1d3c78e191384328b256cabd9
Lucas Saladini <lucassaladini@hotmail.com>
2023-04-20 18:45:12 -0300

    Working with While loop

--- C#/Loops/Program.cs
+++ C#/Loops/Program.cs
@@ -1,6 +1,19 @@
 ﻿int number = 5;
 
-for (int counter = 0; counter <= 10; counter++)
+// for (int counter = 0; counter <= 10; counter++)
+// {
+//     Console.WriteLine($"{number} x {counter} = {number * counter}");
+// }
+
+int counter = 0;
+
+while (counter <= 10)
 {
     Console.WriteLine($"{number} x {counter} = {number * counter}");
+    counter++;
+
+    if (counter == 6)
+    {
+        break;
+    }
 }
