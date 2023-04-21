5ff063030cc463c4e091ba716435b34f61a7ef23
Lucas Saladini <lucassaladini@hotmail.com>
2023-04-21 16:16:30 -0300

    Using foreach on list

---
+++ C#/Lists/Program.cs
@@ -0,0 +1,20 @@
+﻿List<string> listString = new List<string>();
+
+listString.Add("SP");
+listString.Add("BH");
+listString.Add("MG");
+
+Console.WriteLine("Percorrendo a lista com FOR");
+for (int counter = 0; counter < listString.Count; counter++)
+{
+    Console.WriteLine($"Posição Número {counter} - {listString[counter]}");
+}
+
+int counterForeach = 0;
+
+Console.WriteLine("Percorrendo a lista com FOREACH");
+foreach (string item in listString)
+{
+    Console.WriteLine($"Posição Número {counterForeach} - {item}");
+    counterForeach++;
+}
