09102aaff150101bd3987148067a810bebf6deeb
Lucas Saladini <lucassaladini@hotmail.com>
2023-04-21 15:49:22 -0300

    Using FOREACH on Array

--- C#/Array/Program.cs
+++ C#/Array/Program.cs
@@ -4,7 +4,17 @@ arrayIntegers[0] = 72;
 arrayIntegers[1] = 74;
 arrayIntegers[2] = 50;
 
+Console.WriteLine("Percorrendo o Array com o FOR");
 for (int counter = 0; counter < arrayIntegers.Length; counter++)
 {
     Console.WriteLine($"Posição Número {counter} - {arrayIntegers[counter]}");
+}
+
+Console.WriteLine("Percorrendo o Array com o FOREACH");
+
+int counterForeach = 0;
+foreach (int value in arrayIntegers)
+{
+    Console.WriteLine($"Posição Número {counterForeach} - {value}");
+    counterForeach++;
 }
