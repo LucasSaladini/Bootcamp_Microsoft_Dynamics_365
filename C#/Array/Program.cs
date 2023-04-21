932fa87bd81abf3dc9f7286cdbbf465854636d40
Lucas Saladini <lucassaladini@hotmail.com>
2023-04-21 15:35:43 -0300

    Working with arrays

---
+++ C#/Array/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] arrayIntegers = new int[3];
+
+arrayIntegers[0] = 72;
+arrayIntegers[1] = 74;
+arrayIntegers[2] = 50;
+
+for (int counter = 0; counter <= arrayIntegers.Length; counter++)
+{
+    Console.WriteLine($"Posição Número {counter} - {arrayIntegers[counter]}");
+}
