ef8d6222711908f3dd3769c65d4d8217e7d42ecf
Lucas Saladini <lucassaladini@hotmail.com>
2023-04-21 15:36:42 -0300

    Fixed condition

--- C#/Array/Program.cs
+++ C#/Array/Program.cs
@@ -4,7 +4,7 @@ arrayIntegers[0] = 72;
 arrayIntegers[1] = 74;
 arrayIntegers[2] = 50;
 
-for (int counter = 0; counter <= arrayIntegers.Length; counter++)
+for (int counter = 0; counter < arrayIntegers.Length; counter++)
 {
     Console.WriteLine($"Posição Número {counter} - {arrayIntegers[counter]}");
 }
