362773706a6b9f97f3f3db45d67ca8a7ed1a0065
Lucas Saladini <lucassaladini@hotmail.com>
2023-04-22 19:33:54 -0300

    Added method to remove student

--- C#/Properties_Methods_Constructors/Models/Course.cs
+++ C#/Properties_Methods_Constructors/Models/Course.cs
@@ -21,5 +21,10 @@ namespace Properties_Methods_Constructors.Models
             int quantity = Students.Count();
             return quantity;
         }
+
+        public bool RemoveStudent(Person student)
+        {
+            return Students.Remove(student);
+        }
     }
 }
