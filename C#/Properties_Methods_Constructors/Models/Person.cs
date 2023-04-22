61eceb07278306eb61d4cd84037b25f3b019a809
Lucas Saladini <lucassaladini@hotmail.com>
2023-04-22 18:37:13 -0300

    Created Person Model

---
+++ C#/Properties_Methods_Constructors/Models/Person.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Properties_Methods_Constructors.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
