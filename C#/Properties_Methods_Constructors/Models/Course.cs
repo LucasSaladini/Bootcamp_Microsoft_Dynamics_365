aed0bde453de19cafe6ead767ed3d845b56f19ac
Lucas Saladini <lucassaladini@hotmail.com>
2023-04-22 19:24:38 -0300

    Created a class for students and added a method to add a student

---
+++ C#/Properties_Methods_Constructors/Models/Course.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Properties_Methods_Constructors.Models
+{
+    public class Course
+    {
+        public string Name { get; set; }
+
+        public List<Person> Students { get; set; }
+
+        public void AddStudent(Person student)
+        {
+            Students.Add(student);
+        }
+    }
+}
