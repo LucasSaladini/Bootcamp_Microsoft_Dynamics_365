1416313a07d2dbfe607b0e6e1531422a958cee5c
Lucas Saladini <lucassaladini@hotmail.com>
2023-04-29 11:55:33 -0300

    Concat values

--- C#/ValueManipulation/Program.cs
+++ C#/ValueManipulation/Program.cs
@@ -13,3 +13,22 @@ englishCourse.AddStudent(p1);
 englishCourse.AddStudent(p2);
 
 englishCourse.ListStudents();
+
+
+
+string number1 = "10";
+string number2 = "20";
+
+int number3 = 10;
+int number4 = 20;
+
+string number5 = "10";
+int number6 = 20;
+
+string result = number1 + number2; //concat
+int result1 = number3 + number4; //sum
+string result3 = number5 + number6; //concat
+
+Console.WriteLine(result);
+Console.WriteLine(result1);
+Console.WriteLine(result3);
