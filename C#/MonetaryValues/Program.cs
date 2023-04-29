7dc893a7432601038265ab6d99a03c0e954d7c27
Lucas Saladini <lucassaladini@hotmail.com>
2023-04-29 12:07:00 -0300

    Formating monetary values based on culture

---
+++ C#/MonetaryValues/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Globalization;
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+
+decimal monetaryValue = 1582.40M;
+
+Console.WriteLine($"{monetaryValue:C}"); //:C -> Currency
+Console.WriteLine(monetaryValue.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
+Console.WriteLine(monetaryValue.ToString("C8"));
