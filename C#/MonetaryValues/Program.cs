ea3df76ee96b537e97f6631f19a242be62693373
Lucas Saladini <lucassaladini@hotmail.com>
2023-04-29 12:09:04 -0300

    Formating numbers

--- C#/MonetaryValues/Program.cs
+++ C#/MonetaryValues/Program.cs
@@ -7,3 +7,12 @@ decimal monetaryValue = 1582.40M;
 Console.WriteLine($"{monetaryValue:C}"); //:C -> Currency
 Console.WriteLine(monetaryValue.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
 Console.WriteLine(monetaryValue.ToString("C8"));
+
+
+double percent = .3421;
+
+Console.WriteLine(percent.ToString("P2"));
+
+int number = 123456;
+
+Console.WriteLine(number.ToString("##-##-##"));
