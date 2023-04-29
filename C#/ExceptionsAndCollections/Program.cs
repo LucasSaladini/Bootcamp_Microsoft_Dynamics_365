52240427e09a019942aca94ea58f4bb4bff71458
Lucas Saladini <lucassaladini@hotmail.com>
2023-04-29 17:30:02 -0300

    Added generic exception handling

--- C#/ExceptionsAndCollections/Program.cs
+++ C#/ExceptionsAndCollections/Program.cs
@@ -1,6 +1,13 @@
-﻿string[] lines = File.ReadAllLines("files/read_file.txt");
-
-foreach (string line in lines)
+﻿try
 {
-    Console.WriteLine(line);
+    string[] lines = File.ReadAllLines("files/readfile.txt");
+
+    foreach (string line in lines)
+    {
+        Console.WriteLine(line);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ocorreu uma exceção genérica. {ex.Message}");
 }
