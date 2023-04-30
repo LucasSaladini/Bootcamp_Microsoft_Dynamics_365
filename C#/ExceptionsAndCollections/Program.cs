be1b703724147aa1e7253a71afba8732b95efc97
Lucas Saladini <lucassaladini@hotmail.com>
2023-04-30 10:38:10 -0300

    Handling specific exceptions

--- C#/ExceptionsAndCollections/Program.cs
+++ C#/ExceptionsAndCollections/Program.cs
@@ -1,11 +1,19 @@
 ﻿try
 {
-    string[] lines = File.ReadAllLines("files/readfile.txt");
+    string[] lines = File.ReadAllLines("files/read_file.txt");
 
     foreach (string line in lines)
     {
         Console.WriteLine(line);
     }
+}
+catch (FileNotFoundException ex)
+{
+    Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Arquivo não encontrado. {ex.Message}");
+}
+catch (DirectoryNotFoundException ex)
+{
+    Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Caminho da pasta não encontrado. {ex.Message}");
 }
 catch (Exception ex)
 {
